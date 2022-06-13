e1f30d7fc9117ad7a6f299f8f0ddb1f11ed6ffeb
Natalie Goodwin <nmgoodwin27@gmail.com>
2022-06-13 18:04:43 -0500

    additions on June 13 2022

---
+++ weekElevenNotes/week11quiz.js
@@ -0,0 +1,37 @@
+let div = $('table-div');
+let table = $('#customer-table');
+let tr = $('tr');
+let th =  $('th');
+let td = $('td');
+let first = $('first');
+let last =  $('last');
+let email = $('email');
+
+
+tr.append('<td>Sam Smith sam.smith@gmail.com</td>');
+tr.append('<td>Tom Jones tom.jones@gmail.com</td>');
+
+
+
+
+
+/*<input type="text">
+        <p id="test">Hello World</p>
+        <div class="my-class">Div</div>
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+        </ul>
+
+let p = $('#test'); /* jquery element object that is 
+returned from the jquery call*/
+/*let div = $('.my-class');*/
+/*let ul = $('ul');
+
+console.log(p);
+console.log(div);
+console.log(ul);*/
+
+
+
